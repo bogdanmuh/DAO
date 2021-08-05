3fb2e96173e488057d2e1dc658cfd1a8cf7b9bbc
bogdanmuh <bogdanmuhutdinov97@gmail.com>
2021-08-05 19:56:41 +0300

    поключения к бд ,  и сохранения  данных со страницы

--- src/main/java/springcourse/dao/PersonDAO.java
+++ src/main/java/springcourse/dao/PersonDAO.java
@@ -3,40 +3,81 @@ package springcourse.dao;
 import org.springframework.stereotype.Component;
 import springcourse.model.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     private  static  int PEOPLE_COUNT;
-    private List<Person> people;
+    private static final String URL="jdbc:postgresql://localhost:5432/first_db";
+    // лучше засунуть его в propities
+    private  static  final String USERNAME ="postgres";
+    private  static  final String PASSWORD="root";
+    private static Connection connection;
 
-    //блок иницилизации
-    {
-        people= new ArrayList<>();
-        people.add(new Person(PEOPLE_COUNT++,"Tom",22,"Tom@mail.ru"));
-        people.add(new Person(PEOPLE_COUNT++,"Bob",54,"Bob@gmail.com"));
-        people.add(new Person(PEOPLE_COUNT++,"Mike",33,"Mike@yandex.ru"));
-        people.add(new Person(PEOPLE_COUNT++,"Tom",19,"Tom@mail.ru"));
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }// рефлексия
+        try {
+            connection= DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    public List<Person> index(){
+
+
+    public List<Person> index()  {
+
+        List<Person>people= new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL="SELECT * FROM person";
+            ResultSet resultSet=statement.executeQuery(SQL);//метод executeQuery возвращает resultset
+            while(resultSet.next()){
+                Person person= new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+                people.add(person);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return people;
     }
     public Person show (int id ){
-        return people.stream().filter(person ->person.getId()==id).findAny().orElse(null);
+        //return people.stream().filter(person ->person.getId()==id).findAny().orElse(null);
+        return null;
     }
     public  void save (Person person){
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            String SQL1="INSERT INTO Person Values("+1+",`"+person.getName()+"`," +
+                    person.getAge()+",`"+ person.getEmail() +"`)";
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() +
+                    "'," + person.getAge() + ",'" + person.getEmail() + "')";
+
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
     public void update (int id, Person updatePerson){
-        Person personToBeUpdate= show(id);
+        /*Person personToBeUpdate= show(id);
         personToBeUpdate.setName(updatePerson.getName());
         personToBeUpdate.setAge(updatePerson.getAge());
-        personToBeUpdate.setEmail(updatePerson.getEmail());
+        personToBeUpdate.setEmail(updatePerson.getEmail());*/
 
     }
     public  void delete(int id ){
-        people.removeIf(a->a.getId()==id);
+        //people.removeIf(a->a.getId()==id);
     }
 }
